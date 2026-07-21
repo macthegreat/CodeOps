2b2e93d8d26da2552d16f943f6efbd9b27256b87
macthegreat <mekbibk3795@gmail.com>
2026-07-21 22:52:13 +0300

    day 07 completed serching

---
+++ Module-01/Day07/practice.py
@@ -0,0 +1,49 @@
+
+# Account_numbers=["Abebe","Hana"]
+# class Account:
+#     def __init__(self,owner,acc_number):
+#         self.owner=owner
+#         self.acc_number=acc_number
+#         acc_info=[owner,acc_number]
+#         Account_numbers.append(acc_info)
+    
+#     #def account_register(self,owner,acc_number):
+        
+# james=Account("james","AAA-001")
+# jacob=Account("jacob","AAA-002")
+
+# print(f"accounts: {Account_numbers}");  
+        
+# class factorial:
+#     def __init__(self,num1):
+#         self.num1=num1
+
+#         if num1 == 0:
+#             return 1
+#         else:
+#             for _ in range(num1):
+#                 return num1*factorial(num1-1)
+
+
+# print(factorial(0))
+    
+###  
+
+
+
+list=[1,2,3,4,5,6,7,8,9,10]
+def linsearch(list):
+    target=2
+    for i in list:
+       if i == target:  
+        return f"your target is at index:{i}"
+print(linsearch(list))    
+
+
+
+## binary search
+
+
+
+    
+        
